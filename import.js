3ab5fbd8d5326f63f0c77f0194dec367488aaf5d
Karl Davis <karldavis@me.com>
2022-02-20 12:19:36 +0000

    few tweaks

--- import.js
+++ import.js
@@ -3,9 +3,7 @@ let config = {
     rootUrl: 'https://raw.githubusercontent.com/Deek1337/BitburnerScripts/main/',
     serverPrefix: 'DeeK',
   };
-  /*
-   * This will import all files listed in importFiles.
-   */
+  
   export async function main(ns) {
     let filesImported = await importFiles(ns);
     ns.tprint('='.repeat(20));
